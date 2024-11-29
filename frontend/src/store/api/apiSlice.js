cd419561e9f79d4a1a3b2a200b7ceb17731c99fa
Fallinle <167944168+tuannguyenquang22@users.noreply.github.com>
2024-11-29 16:35:26 +0700

    up

--- frontend/src/store/api/apiSlice.js
+++ frontend/src/store/api/apiSlice.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:8080/api/v1"
+        baseUrl: "http://simpleflow_backend:8080/api/v1"
     }),
     tagTypes: ["DataSource", "Learner", "FeatureSet", "LearnerTask"],
     endpoints: (builder) => ({}),
